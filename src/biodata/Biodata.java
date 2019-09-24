318967bb9ef0f3f47506235be5ce6cc15390764e
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 20:47:09 +0700

    Add files via upload

---
+++ src/biodata/Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1 
+ * Deskripsi program : program ini berisi program untuk 
+ * membuat program keluaran / menampilkan data
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Fiqri Akbar Pratama");
+        System.out.println("ALAMAT\t\t: Jl.Seroja IV No.185 Rancaekek kencana");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 13 Mei 2000");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t: Berbisnis & Olahraga");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Capucino");
+    }
+    
+}
